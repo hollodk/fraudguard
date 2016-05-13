cd612a2bb836a762bdeb3cf0b68b17616a65d4d2
Michael Holm <hollo@hollo.dk>
2016-05-13 15:54:02 +0200

    Fixed stuff.

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
-t
-=
+FraudGuard Example
+==================
 
-A Symfony project created on May 13, 2016, 3:03 pm.
+- Start by creating an account at tools.snorestopper.eu/fraudguard/web
+- Add an API key
+- Add key in src/AppBundle/Controller/DefaultController.php
+
+Tests
+-----
+
+Generate page views
+http://localhost/fraudguard/web/
+
+Generate create_order
+http://localhost/fraudguard/web/ok
+
+Callback url
+http://localhost/fraudguard/web/callback
--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -13,9 +14,74 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
-        ]);
+        return $this->render('default/index.html.twig', array(
+            'session_id' => $request->getSession()->getId(),
+            'api_key' => $this->container->getParameter('fraudguard_apikey')
+        ));
+    }
+
+    /**
+     * @Route("/ok", name="ok")
+     */
+    public function okAction(Request $request)
+    {
+        $order = new \StdClass();
+
+        $order->amount = 200;
+        $order->id = 25;
+        $order->user_id = 12;
+        $order->email = 'user@example.com';
+        $order->createdAt = new \DateTime();
+        $order->shipping_method = 'standard';
+        $order->currency = 'DKK';
+
+        $order->firstname = 'Theodor';
+        $order->lastname = 'Jacobsen';
+        $order->address = 'Vesterbro 11';
+        $order->zip = '8000';
+        $order->city = 'Aarhus';
+        $order->country = 'DK';
+
+        $order->shipping = new \StdClass();
+        $order->shipping->firstname = 'Theodor';
+        $order->shipping->lastname = 'Jacobsen';
+        $order->shipping->address = 'Vesterbro 11';
+        $order->shipping->zip = '8000';
+        $order->shipping->city = 'Aarhus';
+        $order->shipping->country = 'DK';
+
+        $order->items = array();
+
+        $item = new \StdClass();
+        $item->category = 'Tech';
+        $item->product_id = 123;
+        $item->unit_price = 12;
+        $item->quantity = 1;
+        $order->items[] = $item;
+
+        $item = new \StdClass();
+        $item->category = 'Bath';
+        $item->product_id = '111';
+        $item->unit_price = 33;
+        $item->quantity = 3;
+        $order->items[] = $item;
+
+        return $this->render('default/ok.html.twig', array(
+            'session_id' => $request->getSession()->getId(),
+            'api_key' => $this->container->getParameter('fraudguard_apikey'),
+            'ip' => $request->getClientIp(),
+            'user_agent' => $request->server->get('HTTP_USER_AGENT'),
+            'accept_language' => $request->server->get('HTTP_ACCEPT_LANGUAGE'),
+            'shop_name' => 'FraudGuard Demo Shop',
+            'order' => $order
+        ));
+    }
+
+    /**
+     * @Route("/callback", name="callback")
+     */
+    public function callbackAction(Request $request)
+    {
+        return new Response('ok');
     }
 }
